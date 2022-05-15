d2c7be32df40a5a282d86d9729964aad8e1ba290
Adelaide <adelaide.craig@outlook.com>
2022-05-15 16:48:33 -0500

    some more

--- src/pages/User.js
+++ src/pages/User.js
@@ -36,15 +36,15 @@ const User = ({user}) => {
 
 
     return(
-        <div className='your-streams'>
+        <div >
             <h3 >Your Streams</h3>
             { streamers.map((streamer) => (
-                   <div onClick={() => showStreamer(streamer)}>
+                   <div onClick={() => showStreamer(streamer)}className='your-streams'>
                    <div className="streamer-details">
                    <img className="streamer-pics" src={streamer.img} alt="streamer" style={{display: 'block', maxWidth:'20%'}} />
                      <h1>{streamer.name}</h1>
                  </div>
-                 <button onClick={() => removeStreamer(streamer.id)}>remove from watchlist</button>
+                 <button onClick={() => removeStreamer(streamer.id)} className="remove">remove from watchlist</button>
                 </div>
             ))}
         </div>
